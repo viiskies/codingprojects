d9ee64ec80523c8f45e0f823096bd26a6e022deb
Vytautas <ali.gatqr@gmail.com>
2017-10-10 15:47:53 +0300

    touching circle changing color

--- canvas2/js/script.js
+++ canvas2/js/script.js
@@ -1,187 +1,165 @@
-canvas = document.getElementById('myCanvas');
+canvas = document.getElementById('canvas');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-function storeGuess(event) {
-	var x = event.offsetX;
-	var y = event.offsetY;
-	guessX = x;
-	guessY = y;
-	console.log("x coords: " + guessX + ", y coords: " + guessY);
-}
-
-
-// var canvasW = 650;
-
-var ctx = canvas.getContext('2d');
-
-
-
-// ctx.rect(400, 500, 400, 400);
-// ctx.fillStyle="#FF0000";
-// ctx.fillRect(400,500,400,400);
-// ctx.stroke();
-
-// ctx.moveTo(400,500);
-// ctx.lineTo(600,300);
-// ctx.lineTo(800,500);
-// ctx.stroke();
-
-
-// ctx.moveTo(540,900);
-// ctx.lineTo(540,700);
-// ctx.lineTo(540,700);
-// ctx.lineTo(660,700);
-// ctx.lineTo(660,900);
-// ctx.stroke();
-
-// ctx.moveTo(728,428);
-// ctx.lineTo(728,328);
-// ctx.lineTo(680,328);
-// ctx.lineTo(680,379);
-
-// ctx.rect(460, 560, 60, 60);
-// ctx.fillStyle="#fff";
-// ctx.fillRect(460,560,60,60);
-// ctx.stroke();
-
-// ctx.rect(680, 560, 60, 60);
-// ctx.fillStyle="#fff";
-// ctx.fillRect(680,560,60,60);
-// ctx.stroke();
-
-// ctx.beginPath();
-// ctx.arc(900,150,40,0,2*Math.PI);
-// ctx.stroke();
-// var coorX = Math.random() * window.innerWidth;
-// var coorY = Math.random() * window.innerHeight;
-
-
-
-// ctx.fillStyle = 'rgb(' + rRed +', ' + rBlue + ', ' + rGreen + ')';
-
-// for (var i = 0; i < 1; i++) {
-
-// 	var circleSize = Math.random() * 90+10;
-// 	var coorX = Math.random() * window.innerWidth;
-// 	var coorY = Math.random() * window.innerHeight;
-
-
-// 	ctx.beginPath();
-// 	ctx.arc(coorX, coorY, circleSize, 0, 2*Math.PI);
-// 	rRed = Math.ceil(Math.random() * 255);
-// 	rBlue = Math.ceil(Math.random() * 255);
-// 	rGreen = Math.ceil(Math.random() * 255);
-
-// 	
-// 	ctx.fill();
-
-// }
-
-
+context = canvas.getContext("2d");
 
 d_x = 0;
 d_y = 0;
 
+var guessX;
+var guessY;
+function storeGuess(event) {
 
-document.onkeydown = function (e) {
-	switch (e.keyCode) {
-		case 37: //left
-			d_x -= 20;
-			break;
-		case 38: //up
-			d_y -= 20;
-			break;
-		case 39: //right
-			d_x += 20;
-			break;
-		case 40: //down
-			d_y += 20;
-			break;
+	var x = event.offsetX;
+	var y = event.offsetY;
+	guessX = x;
+	guessY = y;
 
-	}
 }
+console.log(guessX);
+// var centerX = canvas.width / 2;
+// var centerY = canvas.height / 2;
+// var radius = 70;
 
-function Box(x, y, w, h) {
+// context.beginPath();
+// context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+// context.fillStyle = 'green';
+// context.fill();
 
+
+// document.onkeydown = function(e) {
+// 	switch (e.keyCode) {
+//         case 37: // left
+//         d_x -= 20; 
+//         break;
+//         case 38: // up
+//         d_y -= 20; 
+//         break;
+//         case 39: // right
+//         d_x += 20; 
+//         break;
+//         case 40: //down
+//         d_y += 20;
+//         break;
+//     }
+// };
+
+
+// function touching(dimension, x, y, radius) {
+
+// 	// from top-bottom
+// 	if (
+// 		dimension == "y" && ((y+radius) > box.y) &&
+// 		((y-radius) < (box.y + box.h)) &&
+// 		x > box.x && x <box.x + box.w
+// 		){
+// 		return true;
+// 	}
+
+// 	// from left-right
+// 	if (
+// 		dimension == "x" && ((x+radius) > box.x) &&
+// 		((x-radius) < (box.x + box.h)) &&
+// 		y > box.h && y <box.y + box.h
+// 		){
+// 		return true;
+// 	}
+
+// 	return false;
+
+// }
+
+function Circle(x, y, dx, dy, r) {
 	this.x = x;
 	this.y = y;
-	this.w = w;
-	this.h = h;
-
-	this.update = function(dx, dy) {
-		if (this.x+dx <= 0) {
-			dx = 0;
-			this.x = 0;
-			
-		}
-		if (this.y+dy <= 0) {
-			dy = 0;
-			this.y = 0;
-		}
-		if (this.x+dx >= innerWidth-this.w) {
-			dx = 0;
-			this.x = innerWidth-this.w;
-			
-		}
-		if (this.y+dy >= innerHeight - this.h) {
-			dy = 0;
-			this.y = innerHeight - this.h;
+	this.dx = dx;
+	this.dy = dy;
+	this.r = r;
+	this.update = function() {
+		if (this.x + this.r >= innerWidth || this.x - this.r <= 0) {
+			this.dx = -this.dx;
 		}
 
-		this.x += dx;
-		this.y += dy;
+		if (this.y + this.r >= innerHeight || this.y - this.r <= 0) {
+			this.dy = -this.dy;
+		} 
 
-		this.draw();
-
-	}
-
-
-	this.draw = function () {
-		ctx.beginPath();
-		ctx.rect(this.x, this.y, this.w, this.h);
-		ctx.fillStyle = "black";
-		ctx.fill();
-
-		// if (this.x + this.circleSize >= this.innerWidth || this.x - this.circleSize <= 0) {
+		// if (touching("x",this.x, this.y, this.r)) {
 		// 	this.dx = -this.dx;
 		// }
 
-		// if ( this.y + this.circleSize > this.innerHeight || this.y - this.circleSize < 0) {
+		// if (touching("y",this.x, this.y, this.r)) {
 		// 	this.dy = -this.dy;
 		// }
 
-		// this.x += this.dx;
-		// this.y += this.dy;
+		this.x += this.dx;
+		this.y += this.dy;
 
-		
+		this.draw();
 	}
 
-	this.draw();
+	this.hover = function() {
+		if (this.x - guessX < 0) {
+			console.log(guessX);
+
+		}
+
+	}
+	this.draw = function() {
+		context.beginPath();
+		context.arc(this.x, this.y, this.r, 0,2*Math.PI);
+		context.fillStyle = "violet";
+		context.fill();
+	}
 }
 
+// function Box(x, y, w, h) {
+// 	this.x = x;
+// 	this.y = y;
+// 	this.w = w;
+// 	this.h = h;
 
-var box = new Box(200, 200, 200, 200);
+// 	this.update = function(dx, dy) {
+// 		this.x += dx;
+// 		this.y += dy;
+// 		this.draw();
+// 	}
+// 	this.draw = function() {
+// 		c.beginPath();
+// 		c.rect(this.x, this.y, this.w, this.h);
+// 		c.fillStyle = "black";
+// 		c.fill();
+// 	}
+// }
 
+
+var circleArray = [];
+
+for (var i = 0; i < 1; i++) {
+	var x = Math.floor(Math.random() * (window.innerWidth - 50) + 50);
+	var y = Math.floor(Math.random() * (window.innerHeight - 50) + 50);
+	var dx = (Math.random() - 0.5) * 2;
+	var dy = (Math.random() - 0.5) * 2;
+
+	circleArray.push(new Circle(x,y,dx,dy,10));
+}
+
+// var box = new Box(200,200,100,100);
 
 function animate() {
 	requestAnimationFrame(animate);
-	ctx.clearRect(0, 0, innerWidth, innerHeight);
-	// for(var i = )
-	box.update(d_x, d_y);
-	d_x = 0;
-	d_y = 0;
+	context.clearRect(0,0,innerWidth,innerHeight);
 
+	for (var i = circleArray.length - 1; i >= 0; i--) {
+		circleArray[i].update();
+	}
+	// box.update(d_x, d_y);
+	// d_x = 0;
+	// d_y = 0;
 }
 
 animate();
 
-
-// var grd = ctx.createLinearGradient(400,500,400,400);
-// grd.addColorStop(0,"red");
-// grd.addColorStop(1,"white");
-
-// ctx.fillStyle = grd;
-// ctx.fillRect(400,500,400,400);
 
