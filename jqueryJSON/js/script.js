b17ea3eb49f8263f0b9d5431c435e6035c667721
Vytautas <ali.gatqr@gmail.com>
2017-10-16 10:10:31 +0300

    new json

--- jqueryJSON/js/script.js
+++ jqueryJSON/js/script.js
@@ -25,25 +25,64 @@ $(document).ready(function(){
 
 	});
 
-	$("#get_data_recipes").click(function() {
+$("#get_data_recipes").click(function() {
+ 
+        $.getJSON("http://ca-php.lt/json/recipes.php", function(result){
+ 
+            // thead
+            $("#recipes").append("<thead>");
+            $("#recipes thead").append("<tr>");
+            $("#recipes thead tr").append("<th>NUMBER");
+ 
+            $.each(Object.keys(result[0]), function(index, value) {
+                if( index == 1) {
+                    $("#recipes tr").append("<th colspan='2'>" + value.toUpperCase());
+                } else {
+                    $("#recipes tr").append("<th>" + value.toUpperCase());
+                }
+            });
+ 
+            //  tbody
+            $("#recipes").append("<tbody>");
+ 
+            $.each(result, function(i, field) {
 
-		$.getJSON("http://ca-php.lt/json/recipes.php", function(result){
-			$.each(result, function(i, field){
-				let counter = i + 1;
-				$("#recipes tbody").append("<tr>");
-				$("#recipes tbody").append("<th>" + counter + "</th>");
-				$("#recipes tbody").append("<td>" + result[i].name + "</td>");
-				console.log(result[i].contents);
-				// $(result[i].contents).each(function(j, field) {
-				// 	console.log(result[i].contents[0]);
-				// });
-				$("#recipes tbody").append("<td>" + result[i].time_to_prepare + "</td>");
-				$("#recipes tbody").append("<td>" + result[i].vegan + "</td>");
-				$("#recipes tbody").append("</tr>");
-			});
-		});
+                let counter = i + 1;
+ 
+                let ingredients = [];
 
-	});
+                $.each(field.contents, function(index, value) {
+                    ingredients.push( index );
+                });
+ 
+                let rowsp = "rowspan='" + ingredients.length + "'";
+ 
+                $("#recipes tbody").append(
+ 
+                    "<tr><th " + rowsp + ">" + 
+                    counter + "<td " + rowsp + ">" +
+                    field.name + "<td " + rowsp + ">" +
+                    field.time_to_prepare + "<td " + rowsp + ">" +
+                    field.vegan
+                   
+                    );
+ 
+                let recipeList = '<th>' + ingredients[0] + '<td>' + Object.values(field.contents)[0];
+ 
+                $("#recipes tbody tr:last-child>td:eq(0)").after(recipeList);
+                $.each(ingredients, function(index, value) {
+                    if(index != 0) {
+                        $("#recipes tbody").append (
+ 
+                            '<tr><th>' + value +
+                            '<td>' + Object.values(field.contents)[index]
+ 
+                        );
+                    }
+                });
+            });
+        });
+    });
 
 
 
